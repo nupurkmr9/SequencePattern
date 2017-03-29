e42703e554085b869a291a7d8d876ffb030de991
nupur <nupurkmr9@gmail.com>
2017-03-29 21:51:01 +0530

    fourth

--- gsp.cpp
+++ gsp.cpp
@@ -45,8 +45,9 @@ public:
 	int id;
 
 	void addItemSet(itemset i ){
+		sort(i.items.begin(), i.items.end());
 		itemsets.push_back(i);
-		size += i.size;
+		size += 1;
 	}
 
 	void addItem(int a){
@@ -54,7 +55,7 @@ public:
 		i.addItem(a);
 		itemsets.pop_back();
 		itemsets.push_back(i);
-		size +=1;
+		//size +=1;
 	}
 
 	void Clear(){
@@ -63,16 +64,43 @@ public:
 	}
 
 	int subsequence(vector<itemset> i){
-		//// to do 
+		int index = 0;
+		for ( vector<itemset>::iterator it1 = i.begin() ; it1 < i.end(); it1++ ){
+			int k = index;
+			cout <<'m';
+			cout <<'\n';
+			vector<int> A = it1->items;
+			while(k < itemsets.size()){
+				vector<int> B = itemsets[k].items;
+				if ( includes(B.begin(), B.end(), A.begin(), A.end()) ) {
+					cout <<'j';
+					cout <<'\n';
+					index = k+1;
+					break;
+				}
+				else{
+					cout <<'h';
+					cout <<'\n';
+					k +=1;
+				}
+
+			}
+			if (k >= itemsets.size()){
+				cout << 'i';
+				cout << '\n';
+				return 0;
+			}
+		}
 		return 1;
 	}
 
 };
 
 
+#define structure map<int, int>
 vector<sequence> Data;
 vector<vector<sequence> > F;	
-itemset Allitems;
+structure Allitems;
 int minSupport = 2;
 
 void readInput();
@@ -80,17 +108,22 @@ void output(vector<sequence>);
 void GSP();
 vector<sequence> generateCandidte(int);
 vector<sequence> frequent1Seq();
+int pruned(sequence);
+vector<sequence> join(sequence,sequence);
 
 int main(int argc, char const *argv[]){
 	readInput();
 	output(Data);
-	GSP();
+	int m = Data[7].subsequence(Data[1].itemsets);
+	cout << m;
+	cout << '\n';
+	// GSP();
 
-	for (int k=0; k<F.size(); k++ ){
-		cout << k;
-		cout << '\n';
-		output(F[k]);
-	}
+	// for (int k=0; k<F.size(); k++ ){
+	// 	cout << k;
+	// 	cout << '\n';
+	// 	output(F[k]);
+	// }
 }
 
 
@@ -105,6 +138,7 @@ void readInput(){
 	sequence s;
 	itemset i;
 	int n;
+	structure done;
 	while (fin>>n){
 
 		if (n== -1){
@@ -118,6 +152,7 @@ void readInput(){
 			Data.push_back(s);
 			i.Clear();
 			s.Clear();
+			done.clear();
 			continue;
 		}
 
@@ -125,8 +160,13 @@ void readInput(){
 			i.addItem(n);
 			//cout << n ;
 		}
-		if (Allitems.notExists(n)){
-			Allitems.addItem(n);
+		
+		if (!(done.count(n) > 0)){
+			if(Allitems.count(n)>0)
+				Allitems[n]++;
+			else
+				Allitems[n]=1;
+			done[n]=1;
 		}
 
 	}
@@ -155,7 +195,7 @@ void GSP(){
 	///////// count most frequent items and intialise F[1] with it /////////
 	F.push_back(frequent1Seq());
 	
-	//////// k frequent sequences while loop //////////////
+	//////// k frequent sequence while loop //////////////
 	int k = 2;
 	while( !F[k-1].empty() ){
 
@@ -176,7 +216,7 @@ void GSP(){
 			}
 		}
 
-		///////////// if count > minsupport keep it as frequent sequence //////////////////
+		///////////// if count > minSupport keep it as frequent sequence //////////////////
 		vector<sequence> FK ;
 		int counter =0;
 		for ( vector<sequence>::iterator c1 = C.begin() ; c1 < C.end(); c1++ ){
@@ -197,9 +237,41 @@ void GSP(){
 
 
 vector<sequence> generateCandidte(int k){
+	vector<sequence> candidates;
+	for ( vector<sequence>::iterator ii = F[k-1].begin() ; ii < F[k-1].end(); ii++ ){
+		for ( vector<sequence>::iterator tt = F[k-1].begin() ; tt < F[k-1].end(); tt++ ){
+			vector<sequence> candidate = join(*ii,*tt);
+			if (candidate.size() > 0){
+				for ( vector<sequence>::iterator cc = candidate.begin() ; cc < candidate.end(); cc++ ){
+					if (!pruned(*cc)){
+						candidates.push_back(*cc);
+					}
+				}
+			}
+		}
+	}
+	return candidates;
+}
 
+vector<sequence> join(sequence c1, sequence c2){
+//// to do ////////
+}
+
+int pruned(sequence c){
+//// to do ////////
 }
 
 vector<sequence> frequent1Seq(){
+	vector <sequence> F1 ;
+	for ( structure::iterator ii = Allitems.begin() ; ii != Allitems.end(); ii++ ){
+		if (ii->second >= minSupport){
+			itemset i;
+			i.addItem(ii->first);
+			sequence s;
+			s.addItemSet(i);
+			F1.push_back(s);
+		}
+	}
+	return F1;
 
 }
